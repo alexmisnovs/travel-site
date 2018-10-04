471d0efe9e937e5e523816ba45e75db3958dc0cf
Alex MacBook air <aleksandrmisnov@gmail.com>
2018-10-04 21:35:16 +0100

    updated gulpfile to watch for changes, create dummy style sheet

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,25 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+watch = require('gulp-watch');
+
 
 gulp.task('default', function(){
     console.log('Hooray gulp is running');
+});
+
+gulp.task('html', function(){
+    console.log('HTML related gulp task');
+
+});
+gulp.task('styles', function(){
+    console.log('CSS related gulp task');
+
+});
+
+gulp.task('watch', function(){
+    watch('./app/index.html', function(){
+        gulp.start('html');
+    });
+    watch('./app/assets/styles/**/*.css', function(){
+        gulp.start('styles');
+    });
 });
