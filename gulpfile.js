ef4e170370e6efd403fb600fd880f1472f50d080
Alex MacBook air <aleksandrmisnov@gmail.com>
2018-10-05 20:35:52 +0100

    added browserSync for development and updated the gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,10 @@ PostCSS = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-postcss_import = require('postcss-import');
+postcss_import = require('postcss-import'),
+browserSync = require('browser-sync').create();
 
 
-gulp.task('default', function(){
-    console.log('Hooray gulp is running');
-});
-
-gulp.task('html', function(){
-    console.log('HTML related gulp task');
-
-});
 gulp.task('styles', function(){
     // PostCSS related logic
     return gulp.src('./app/assets/styles/styles.css')
@@ -29,10 +22,24 @@ gulp.task('styles', function(){
 });
 
 gulp.task('watch', function(){
+
+    browserSync.init({
+        notify:false, // disables notifications
+        server: {
+            baseDir: "app"
+        }
+    });
+
     watch('./app/index.html', function(){
-        gulp.start('html');
+        browserSync.reload();
     });
     watch('./app/assets/styles/**/*.css', function(){
-        gulp.start('styles');
+        gulp.start('cssInject');
     });
+    
+});
+// css browserSync injection
+gulp.task('cssInject',['styles'] ,function(){
+   return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
 });
