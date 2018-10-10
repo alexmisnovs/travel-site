24f09e7f51a617d4c105590843771bb5563f5d93
Alex MacBook air <aleksandrmisnov@gmail.com>
2018-10-10 19:11:07 +0100

    Babel and ES6 practice

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,14 +1,16 @@
 var $ = require('jquery');
- Person = require('./modules/Person');
+//People = require('./modules/Person'); old way of importing the module
+import {Person, Adult} from './modules/Person';
 
  console.log('scripts bundled automatically');
 
 var john = new Person('John', 'Blue');
-
+// have to do this if you export multiple classes from the same module. Confusuin is because I used fileName Person
 john.greet();
 
-var jane = new Person('Jane', 'Orange');
+var jane = new Adult('Jane', 'Orange');
 
-jane.greet();   
+jane.greet(); 
+jane.payTaxes();  
 
 $('h1').remove();
