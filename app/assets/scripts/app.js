c5de6e4c7851034d97ab62c97c4c301ced774ae8
Alex MacBook air <aleksandrmisnov@gmail.com>
2018-10-12 09:07:46 +0100

    Added smooth scroll and other visual effects for the menu

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -3,7 +3,9 @@
 //import {Person, Adult} from './modules/Person';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 
 var mobileMenu = new MobileMenu();
 var revealOnScrollFeatures = new RevealOnScroll(".feature-item", "85%");
 var revealOnScrollTestimonials = new RevealOnScroll(".testimonial", "65%");
+var stickyHeader = new StickyHeader();
