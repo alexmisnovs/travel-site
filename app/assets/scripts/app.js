451c9aa607f021a90bfd5dd6dd5d8a97d2d38ef3
Alex MacBook air <aleksandrmisnov@gmail.com>
2018-10-12 10:36:47 +0100

    completed modal overlay

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -4,8 +4,10 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 var mobileMenu = new MobileMenu();
 var revealOnScrollFeatures = new RevealOnScroll(".feature-item", "85%");
 var revealOnScrollTestimonials = new RevealOnScroll(".testimonial", "65%");
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
